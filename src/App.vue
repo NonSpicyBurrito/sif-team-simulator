3662eed6561e660ef076bf8282ddcd0d347cc0bb
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-02 06:20:37 +0800

    Added about

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import About from './views/About.vue'
 import Simulator from './views/Simulator.vue'
+
+const tabs = {
+    Simulator,
+    About,
+} as const
+
+const current = ref<keyof typeof tabs>('Simulator')
 </script>
 
 <template>
     <div class="p-4">
-        <div
-            class="mt-4 mb-8 text-2xl font-bold text-center sm:mb-16 sm:text-5xl"
-        >
-            SIF Team Simulator
+        <div class="mt-4 mb-8 sm:mb-16">
+            <div class="text-2xl font-bold text-center sm:text-5xl">
+                SIF Team Simulator
+            </div>
+
+            <div class="flex mx-auto mt-4 max-w-lg sm:mt-8">
+                <a
+                    v-for="(value, name) in tabs"
+                    :key="name"
+                    class="basis-0 grow p-2 text-center cursor-pointer sm:p-4"
+                    :class="{ 'border-b-2 ': current === name }"
+                    @click="current = name"
+                >
+                    {{ name }}
+                </a>
+            </div>
         </div>
 
-        <Simulator />
+        <component :is="tabs[current]" />
     </div>
 </template>
---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Field from '../components/Field.vue'
+</script>
+
+<template>
+    <div class="mx-auto max-w-4xl">
+        <Field label="Source Code">
+            <div class="py-1 sm:text-center">
+                <a
+                    class="underline cursor-pointer"
+                    href="https://github.com/NonSpicyBurrito/sif-team-simulator"
+                    target="__blank"
+                >
+                    https://github.com/NonSpicyBurrito/sif-team-simulator
+                </a>
+            </div>
+        </Field>
+    </div>
+</template>
