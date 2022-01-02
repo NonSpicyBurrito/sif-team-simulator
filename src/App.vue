bc01deb05efde119cf069694a9fe2e2b0ab015a9
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-03 05:17:13 +0800

    Improved styling

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,25 @@ const current = ref<keyof typeof tabs>('Simulator')
 </script>
 
 <template>
-    <div class="p-4">
-        <div class="mt-4 mb-8 sm:mb-16">
-            <div class="text-2xl font-bold text-center sm:text-5xl">
-                SIF Team Simulator
-            </div>
-
-            <div class="flex mx-auto mt-4 max-w-lg sm:mt-8">
-                <a
-                    v-for="(value, name) in tabs"
-                    :key="name"
-                    class="basis-0 grow p-2 text-center cursor-pointer sm:p-4"
-                    :class="{ 'border-b-2 ': current === name }"
-                    @click="current = name"
-                >
-                    {{ name }}
-                </a>
-            </div>
+    <div class="pt-4 bg-white/5 shadow-lg sm:pt-8 sm:mb-4">
+        <div class="text-2xl font-bold text-center sm:text-5xl">
+            SIF Team Simulator
         </div>
 
+        <div class="flex mx-auto mt-4 max-w-lg sm:mt-8">
+            <a
+                v-for="(value, name) in tabs"
+                :key="name"
+                class="basis-0 grow p-2 text-center cursor-pointer sm:p-4"
+                :class="{ 'border-b-2 ': current === name }"
+                @click="current = name"
+            >
+                {{ name }}
+            </a>
+        </div>
+    </div>
+
+    <div class="p-4 sm:mb-4">
         <component :is="tabs[current]" />
     </div>
 </template>
