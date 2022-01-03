1e0e258c6c896466f73348a09860f4b61dba418e
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-03 22:51:34 +0800

    Changed to search

--- src/components/CardSelector.vue
+++ src/components/CardSelector.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { useLocalStorage } from '@vueuse/core'
+import { useDebounce, useInterval, useLocalStorage } from '@vueuse/core'
+import { computed, ref, watch } from 'vue'
 import { cards, characters } from '../database'
+import { EffectType, TriggerType } from '../database/Skill'
 import Card from './Card.vue'
 import Field from './Field.vue'
 
@@ -8,41 +10,120 @@ defineEmits<{
     (e: 'select', id: number): void
 }>()
 
-const attributes = useLocalStorage('teamPanel.attribute', [true, false, false])
-const character = useLocalStorage('teamPanel.character', 1)
+const search = useLocalStorage('cardSelector.search', '')
+const debouncedSearch = useDebounce(search, 500)
 
-const characterIds = [
-    ...new Set([...cards.values()].map(({ character }) => character)),
-].sort((a, b) => a - b)
+const attributes = ['smile', 'pure', 'cool']
+const triggers = enumKeys(TriggerType)
+const effects = enumKeys(EffectType)
+const specificTriggers = {
+    s: TriggerType.Time,
+    n: TriggerType.Note,
+    c: TriggerType.Combo,
+    p: TriggerType.Perfect,
+} as Record<string, TriggerType>
+
+const ids = computed(() => {
+    const conditions = debouncedSearch.value.trim().toLowerCase().split(' ')
+    if (!conditions[0]) return []
+
+    return conditions.reduce(
+        (ids, condition) => {
+            if (attributes.includes(condition)) {
+                return ids.filter(
+                    (id) =>
+                        cards.get(id)?.attribute ===
+                        attributes.indexOf(condition)
+                )
+            }
+
+            if (triggers[condition]) {
+                return ids.filter(
+                    (id) =>
+                        cards.get(id)?.skill.trigger.type ===
+                        triggers[condition]
+                )
+            }
+
+            if (effects[condition]) {
+                return ids.filter(
+                    (id) =>
+                        cards.get(id)?.skill.effect.type === effects[condition]
+                )
+            }
+
+            const value = +condition.slice(0, -1)
+            const type = specificTriggers[condition.slice(-1)]
+            if (value && type) {
+                return ids.filter(
+                    (id) =>
+                        cards.get(id)?.skill.trigger.type === type &&
+                        cards.get(id)?.skill.trigger.values[0] === value
+                )
+            }
+
+            const allowedCharacters = [...characters.entries()]
+                .filter(([, name]) => name.toLowerCase().includes(condition))
+                .map(([character]) => character)
+            return ids.filter((id) =>
+                allowedCharacters.includes(cards.get(id)?.character || -1)
+            )
+        },
+        [...cards.keys()]
+    )
+})
+
+const start = ref(0)
+const counter = useInterval(50)
+
+watch(debouncedSearch, () => (start.value = counter.value))
+
+const displayedIds = computed(() =>
+    ids.value.slice(0, (counter.value - start.value) * 10)
+)
+
+function enumKeys<T>(enumType: Record<string, string | T>) {
+    return Object.fromEntries(
+        Object.entries(enumType)
+            .filter(([, value]) => typeof value === 'number')
+            .map(([key, value]) => [key.toLowerCase(), value] as [string, T])
+    )
+}
 </script>
 
 <template>
-    <Field label="Attribute">
-        <button
-            v-for="(name, index) in ['Smile', 'Pure', 'Cool']"
-            :key="index"
-            class="mr-1"
-            :class="{ 'opacity-25': !attributes[index] }"
-            @click="attributes[index] = !attributes[index]"
-        >
-            {{ name }}
-        </button>
-    </Field>
-    <Field label="Character">
-        <select v-model="character" class="w-full">
-            <option :value="-1">(Any)</option>
-            <option v-for="id in characterIds" :key="id" :value="id">
-                {{ characters.get(id) || id }}
-            </option>
-        </select>
+    <Field label="Search">
+        <input v-model="search" class="w-full" type="text" />
+        <div class="my-1 text-sm">
+            <p>
+                <span class="font-semibold">Attributes:</span>
+                {{ attributes.join(', ') }}
+            </p>
+            <p>
+                <span class="font-semibold">Specific Triggers:</span>
+                {{
+                    Object.keys(specificTriggers)
+                        .map((t) => `20${t}`)
+                        .join(', ')
+                }}
+            </p>
+            <p>
+                <span class="font-semibold">Triggers:</span>
+                {{ Object.keys(triggers).join(', ') }}
+            </p>
+            <p>
+                <span class="font-semibold">Effects:</span>
+                {{ Object.keys(effects).join(', ') }}
+            </p>
+            <p>
+                <span class="font-semibold">Character Names:</span>
+                any part of a character's name
+            </p>
+        </div>
     </Field>
     <div class="flex flex-wrap justify-center mt-8 mb-4">
-        <template v-for="id in cards.keys()" :key="id">
+        <template v-for="id in displayedIds" :key="id">
             <Card
-                v-if="
-                    attributes[cards.get(id)!.attribute] &&
-                    (character === -1 || cards.get(id)?.character === character)
-                "
                 class="shrink-0 m-1 cursor-pointer"
                 :="{ id }"
                 @click="$emit('select', id)"
