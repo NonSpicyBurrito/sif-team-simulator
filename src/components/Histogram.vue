8c8181b8f64ccd09f45f01c0d9f19e02a29c463b
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-04 00:16:27 +0800

    Improved histogram styling

--- src/components/Histogram.vue
+++ src/components/Histogram.vue
@@ -9,21 +9,17 @@ defineProps<{
 
 <template>
     <div
-        class="relative mt-16 h-48 border-b-2 border-l-2 border-gray-300 sm:h-96"
+        class="flex relative mt-16 h-48 border-b-2 border-l-2 border-gray-300 sm:h-96"
     >
-        <div v-for="(value, i) in data.histogram" :key="i" class="group">
+        <div
+            v-for="(value, i) in data.histogram"
+            :key="i"
+            class="group flex basis-0 flex-col grow justify-end h-full"
+        >
             <div
-                class="absolute top-0 bottom-0"
-                :style="{
-                    left: `${(100 * i) / data.histogram.length}%`,
-                    width: `${100 / data.histogram.length}%`,
-                }"
-            >
-                <div
-                    class="absolute right-0 bottom-0 left-0 bg-gray-700 group-hover:bg-gray-600"
-                    :style="{ height: `${95 * value}%` }"
-                />
-            </div>
+                class="bg-gray-700 group-hover:bg-gray-600"
+                :style="{ height: `${95 * value}%` }"
+            />
             <div class="hidden group-hover:block absolute top-0 right-0">
                 {{
                     formatter(
