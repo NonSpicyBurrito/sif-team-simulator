b1df6ce5ca8bfd926a667b609a8e2db97fb3d6e6
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-11 12:08:48 +0800

    Added center effect

--- src/core/stats.ts
+++ src/core/stats.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import { accessories, cards, charts, sises } from '../database'
+import { CenterEffectType, CenterSkill } from '../database/Center'
 import { Member, Team } from './Team'
 
 function getRawMemberStats(member: Member, memoryGalleryBonus: number[]) {
@@ -29,8 +30,8 @@ export function calculateTeamStat(
     chartId: string,
     guestCenter: number
 ) {
-    const { attribute: chartAttribute } = charts.get(chartId)!
-    const { attribute: centerAttribute, center } = cards.get(team[4].card.id)!
+    const { attribute } = charts.get(chartId)!
+    const { center } = cards.get(team[4].card.id)!
 
     const results = [...Array(3)].map(() => ({
         base: 0,
@@ -48,31 +49,25 @@ export function calculateTeamStat(
             const sis = sises.get(name)!
             switch (sis.type) {
                 case 'self':
-                    selfSisMultipliers[i][chartAttribute] += sis.value
+                    selfSisMultipliers[i][attribute] += sis.value
                     break
                 case 'team':
-                    teamSisMultiplier[chartAttribute] += sis.value
+                    teamSisMultiplier[attribute] += sis.value
                     break
                 case 'flat':
-                    flatSisBonuses[i][chartAttribute] += sis.value
+                    flatSisBonuses[i][attribute] += sis.value
                     break
                 case 'plock':
-                    trickMultipliers[i][chartAttribute] = sis.value
+                    trickMultipliers[i][attribute] = sis.value
                     break
             }
         })
     )
 
-    centerMultiplier[chartAttribute] += guestCenter
-    centerMultiplier[centerAttribute] +=
-        (center.main.value + (center.extra.value || 0)) / 100
+    centerMultiplier[attribute] += guestCenter
 
-    team.forEach((member, i) => {
-        const raws = getRawMemberStats(member, memoryGalleryBonus)
-
-        results.forEach((result, j) => {
-            const raw = raws[j]
-
+    const intermediate = team.map((member, i) =>
+        getRawMemberStats(member, memoryGalleryBonus).map((raw, j) => {
             const accessoryBonus = member.accessory
                 ? accessories.get(member.accessory.id)!.stats[
                       member.accessory.level - 1
@@ -82,15 +77,86 @@ export function calculateTeamStat(
             const panel =
                 (raw + accessoryBonus) * (1 + selfSisMultipliers[i][j]) +
                 flatSisBonuses[i][j]
-            const affected =
-                panel * (1 + teamSisMultiplier[j] + centerMultiplier[j])
 
-            result.base +=
-                (raw * teamSisMultiplier[j] + panel) * (1 + centerMultiplier[j])
-            result.param += affected
-            result.trick += affected * trickMultipliers[i][j]
+            return {
+                panel,
+                affected: panel * (1 + teamSisMultiplier[j]),
+                base: raw * teamSisMultiplier[j] + panel,
+            }
         })
-    })
+    )
+
+    intermediate.forEach((member, i) =>
+        member.forEach(({ panel, affected, base }, j) => {
+            base +=
+                base * centerMultiplier[j] +
+                applyCenter(
+                    center,
+                    member.map(({ base }) => base),
+                    j
+                )
+            affected +=
+                panel * centerMultiplier[j] +
+                applyCenter(
+                    center,
+                    member.map(({ panel }) => panel),
+                    j
+                )
+
+            results[j].base += base
+            results[j].param += affected
+            results[j].trick += affected * trickMultipliers[i][j]
+        })
+    )
 
     return results
+}
+
+function applyCenter(center: CenterSkill, stats: number[], attribute: number) {
+    return (
+        applyCenterEffect(
+            center.main.type,
+            center.main.value / 100,
+            stats,
+            attribute
+        ) +
+        (center.extra.type && center.extra.value
+            ? applyCenterEffect(
+                  center.extra.type,
+                  center.extra.value / 100,
+                  stats,
+                  attribute
+              )
+            : 0)
+    )
+}
+
+function applyCenterEffect(
+    type: CenterEffectType,
+    value: number,
+    stats: number[],
+    attribute: number
+) {
+    switch (type) {
+        case CenterEffectType.SmilePrincess:
+            return [1, 0, 0][attribute] * value * stats[0]
+        case CenterEffectType.PureAngel:
+            return [0, 1, 0][attribute] * value * stats[1]
+        case CenterEffectType.CoolEmpress:
+            return [0, 0, 1][attribute] * value * stats[2]
+        case CenterEffectType.PurePrincess:
+            return [0, 1, 0][attribute] * value * stats[0]
+        case CenterEffectType.CoolPrincess:
+            return [0, 0, 1][attribute] * value * stats[0]
+        case CenterEffectType.SmileAngel:
+            return [1, 0, 0][attribute] * value * stats[1]
+        case CenterEffectType.CoolAngel:
+            return [0, 0, 1][attribute] * value * stats[1]
+        case CenterEffectType.SmileEmpress:
+            return [1, 0, 0][attribute] * value * stats[2]
+        case CenterEffectType.PureEmpress:
+            return [0, 1, 0][attribute] * value * stats[2]
+        default:
+            throw `Unsupported center effect: ${CenterEffectType[type] || type}`
+    }
 }
--- src/database/Center.ts
+++ src/database/Center.ts
@@ -1,11 +1,23 @@
+export enum CenterEffectType {
+    SmilePrincess = 1,
+    PureAngel = 2,
+    CoolEmpress = 3,
+    PurePrincess = 112,
+    CoolPrincess = 113,
+    SmileAngel = 121,
+    CoolAngel = 123,
+    SmileEmpress = 131,
+    PureEmpress = 132,
+}
+
 export type CenterSkill = {
     main: {
-        type: unknown
+        type: CenterEffectType
         value: number
     }
     extra: {
         apply?: unknown
-        type?: unknown
+        type?: CenterEffectType
         value?: number
     }
 }
