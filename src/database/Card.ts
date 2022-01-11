96b5bad7bddae67e7aebdf08188de2fc10700c53
NonSpicyBurrito <burritoyangster@gmail.com>
2022-01-11 13:43:18 +0800

    Added card group and subunit

--- src/database/Card.ts
+++ src/database/Card.ts
@@ -1,6 +1,24 @@
 import { CenterSkill } from './Center'
 import { CardSkill } from './Skill'
 
+export enum Group {
+    None = 0,
+    Muse = 1,
+    Aqours = 2,
+    Nijigasaki = 3,
+    Liella = 4,
+}
+
+export enum Subunit {
+    None = 0,
+    Printemps = 6,
+    LilyWhite = 7,
+    BiBi = 8,
+    CYaRon = 9,
+    AZALEA = 10,
+    GuiltyKiss = 11,
+}
+
 export enum Rarity {
     R = 2,
     SR = 3,
@@ -10,6 +28,9 @@ export enum Rarity {
 
 export type Card = {
     character: number
+    group: Group
+    year: number
+    subunit: Subunit
     rarity: Rarity
     stats: number[]
     hp: number
