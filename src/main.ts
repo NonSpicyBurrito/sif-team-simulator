463b186c6b104e6d789795cc9b70f2cde40ea39e
jackiwi <jacqueline3aquino@yahoo.com>
2022-03-10 10:57:35 -0500

    revert floating-vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import FloatingVue from 'floating-vue'
-import 'floating-vue/dist/style.css'
 
-createApp(App).use(FloatingVue).mount('#app')
+createApp(App).mount('#app')
