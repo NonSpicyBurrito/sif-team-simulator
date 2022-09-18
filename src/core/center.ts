f75a9e081512e4fa83d5e808c905ea9566392e05
NonSpicyBurrito <burritoyangster@gmail.com>
2022-09-18 19:29:32 +0800

    Added missing party centers

--- src/core/center.ts
+++ src/core/center.ts
@@ -87,18 +87,30 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Honoka and Eli'
         case CenterApplyType.HonokaKotori:
             return 'Honoka and Kotori'
+        case CenterApplyType.HonokaUmi:
+            return 'Honoka and Umi'
+        case CenterApplyType.HonokaRin:
+            return 'Honoka and Rin'
         case CenterApplyType.HonokaMaki:
             return 'Honoka and Maki'
+        case CenterApplyType.HonokaNozomi:
+            return 'Honoka and Nozomi'
         case CenterApplyType.HonokaHanayo:
             return 'Honoka and Hanayo'
         case CenterApplyType.HonokaNico:
             return 'Honoka and Nico'
+        case CenterApplyType.EliKotori:
+            return 'Eli and Kotori'
         case CenterApplyType.EliUmi:
             return 'Eli and Umi'
+        case CenterApplyType.EliRin:
+            return 'Eli and Rin'
         case CenterApplyType.EliMaki:
             return 'Eli and Maki'
         case CenterApplyType.EliNozomi:
             return 'Eli and Nozomi'
+        case CenterApplyType.EliHanayo:
+            return 'Eli and Hanayo'
         case CenterApplyType.EliNico:
             return 'Eli and Nico'
         case CenterApplyType.KotoriUmi:
@@ -107,22 +119,32 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Kotori and Rin'
         case CenterApplyType.KotoriMaki:
             return 'Kotori and Maki'
+        case CenterApplyType.KotoriNozomi:
+            return 'Kotori and Nozomi'
         case CenterApplyType.KotoriHanayo:
             return 'Kotori and Hanayo'
         case CenterApplyType.KotoriNico:
             return 'Kotori and Nico'
         case CenterApplyType.UmiRin:
             return 'Umi and Rin'
+        case CenterApplyType.UmiMaki:
+            return 'Umi and Maki'
         case CenterApplyType.UmiNozomi:
             return 'Umi and Nozomi'
         case CenterApplyType.UmiHanayo:
             return 'Umi and Hanayo'
+        case CenterApplyType.UmiNico:
+            return 'Umi and Nico'
         case CenterApplyType.RinMaki:
             return 'Rin and Maki'
         case CenterApplyType.RinNozomi:
             return 'Rin and Nozomi'
         case CenterApplyType.RinHanayo:
             return 'Rin and Hanayo'
+        case CenterApplyType.RinNico:
+            return 'Rin and Nico'
+        case CenterApplyType.MakiNozomi:
+            return 'Maki and Nozomi'
         case CenterApplyType.MakiHanayo:
             return 'Maki and Hanayo'
         case CenterApplyType.MakiNico:
@@ -133,8 +155,12 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Nozomi and Nico'
         case CenterApplyType.HanayoNico:
             return 'Hanayo and Nico'
+        case CenterApplyType.ChikaRiko:
+            return 'Chika and Riko'
         case CenterApplyType.ChikaKanan:
             return 'Chika and Kanan'
+        case CenterApplyType.ChikaDia:
+            return 'Chika and Dia'
         case CenterApplyType.ChikaYou:
             return 'Chika and You'
         case CenterApplyType.ChikaYoshiko:
@@ -143,6 +169,8 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Chika and Hanamaru'
         case CenterApplyType.ChikaMari:
             return 'Chika and Mari'
+        case CenterApplyType.ChikaRuby:
+            return 'Chika and Ruby'
         case CenterApplyType.RikoKanan:
             return 'Riko and Kanan'
         case CenterApplyType.RikoDia:
@@ -155,10 +183,16 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Riko and Hanamaru'
         case CenterApplyType.RikoMari:
             return 'Riko and Mari'
+        case CenterApplyType.RikoRuby:
+            return 'Riko and Ruby'
         case CenterApplyType.KananDia:
             return 'Kanan and Dia'
         case CenterApplyType.KananYou:
             return 'Kanan and You'
+        case CenterApplyType.KananYoshiko:
+            return 'Kanan and Yoshiko'
+        case CenterApplyType.KananHanamaru:
+            return 'Kanan and Hanamaru'
         case CenterApplyType.KananMari:
             return 'Kanan and Mari'
         case CenterApplyType.KananRuby:
@@ -167,10 +201,14 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'Dia and You'
         case CenterApplyType.DiaYoshiko:
             return 'Dia and Yoshiko'
+        case CenterApplyType.DiaHanamaru:
+            return 'Dia and Hanamaru'
         case CenterApplyType.DiaMari:
             return 'Dia and Mari'
         case CenterApplyType.DiaRuby:
             return 'Dia and Ruby'
+        case CenterApplyType.YouYoshiko:
+            return 'You and Yoshiko'
         case CenterApplyType.YouHanamaru:
             return 'You and Hanamaru'
         case CenterApplyType.YouMari:
@@ -179,10 +217,16 @@ function getApplyDescription(apply: CenterApplyType) {
             return 'You and Ruby'
         case CenterApplyType.YoshikoHanamaru:
             return 'Yoshiko and Hanamaru'
+        case CenterApplyType.YoshikoMari:
+            return 'Yoshiko and Mari'
         case CenterApplyType.YoshikoRuby:
             return 'Yoshiko and Ruby'
+        case CenterApplyType.HanamaruMari:
+            return 'Hanamaru and Mari'
         case CenterApplyType.HanamaruRuby:
             return 'Hanamaru and Ruby'
+        case CenterApplyType.MariRuby:
+            return 'Mari and Ruby'
         case CenterApplyType.Liella:
             return 'Liella'
         default:
